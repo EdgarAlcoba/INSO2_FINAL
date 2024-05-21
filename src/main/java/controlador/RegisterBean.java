bf2d6f6e1153bbbf276d51a99f20c78cb05941b4
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-05-21 23:07:16 +0200

    Terminada logica de navegacion y de errores en el registro

--- src/main/java/controlador/RegisterBean.java
+++ src/main/java/controlador/RegisterBean.java
@@ -13,6 +13,9 @@ import modelo.Usuario;
 
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 /**
  *
  * @author extre
@@ -20,17 +23,19 @@ import java.util.regex.Matcher;
 @ManagedBean
 @RequestScoped
 public class RegisterBean {
+
     private Usuario user = new Usuario();
     private String confirmPassword;
-    
-    private static final String EMAIL_REGEX =  "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-    
+
     @EJB
     private UsuarioFacadeLocal UFL;
 
     public RegisterBean() {
     }
+
     public Usuario getUser() {
         return user;
     }
@@ -46,39 +51,54 @@ public class RegisterBean {
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
     }
-    
-    public void register(){
-        if(!checkPassword()){
+
+    public String register() {
+        if (!checkPassword()) {
             //TODO avisar de que la contraseña esta mal
-        }else if(!isValidEmail()){
-            //TODO avisar ed email defectuoso
-        }else{
-            this.user.setRol("cliente");
+            showAlert(2, "Error en la contraseña","Las contraseñas introducidas no coinciden");
+        } else if (!isValidEmail()) {
+            showAlert(2, "Error en el correo","El correo introducido no es un correo valido");
+        } else {
+            this.user.setRol("Client");
             int newUser = UFL.register(user);
-            switch(newUser){
+            switch (newUser) {
                 case 0:
-                    //TODO mover a login
-                    break;
+                    return "login";
                 case 1:
-                    //TODO email ya existe
+                    showAlert(2, "Error al registrar el usuario", "Ya existe un ususario con este correo");
                     break;
                 case 2:
-                    //TODO fallo registro
+                    showAlert(3, "Error al registrar el usuario", "Ha ocurrido un error fatal al registar al usuario, por favor, espere un minuto y vuelva a intentarlo");
                     break;
             }
         }
+        return "";
     }
-    
-    private boolean checkPassword(){
+
+    private boolean checkPassword() {
         return this.user.getContrasena().equals(this.confirmPassword);
     }
-    
-    private boolean isValidEmail(){
-        if(this.user.getCorreo() == null){
+
+    private boolean isValidEmail() {
+        if (this.user.getCorreo() == null) {
             return false;
         }
         Matcher matcher = EMAIL_PATTERN.matcher(this.user.getCorreo());
         return matcher.matches();
+    }
+
+    private void showAlert(int severity, String name, String text) {
+        System.out.println(">Llega aqui");
+        FacesContext context = FacesContext.getCurrentInstance();
+        switch (severity){
+            case 2:
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, name, text));
+                break;
+            case 3:
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, name, text));
+                break;
+        }
+        
     }
 
 }

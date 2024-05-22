6ea6d81e6fe2cb79a0342c2cf071306a64435ffa
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-05-22 20:05:05 +0200

    Reconfiguracion de los controladores para mejor acceso futuro

--- src/main/java/ControllerWebAcces/RegisterBean.java
+++ src/main/java/ControllerWebAcces/RegisterBean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controlador;
+package ControllerWebAcces;
 
 import EJB.UsuarioFacadeLocal;
 import javax.ejb.EJB;

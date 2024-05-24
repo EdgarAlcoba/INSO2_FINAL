925d40847675551d08c69cae4dd9d15b059b4708
Víctor Martínez <vmartc06@estudiantes.unileon.es>
2024-05-24 22:41:31 +0200

    Agregado metodo mostrar vuelos

--- src/main/java/EJB/VueloFacade.java
+++ src/main/java/EJB/VueloFacade.java
@@ -8,7 +8,17 @@ package EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import modelo.Usuario;
 import modelo.Vuelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -28,5 +38,36 @@ public class VueloFacade extends AbstractFacade<Vuelo> implements VueloFacadeLoc
     public VueloFacade() {
         super(Vuelo.class);
     }
-    
+
+    @Override
+    public ArrayList<Vuelo> searchBetween(Date from, Date to) {
+        if (from == null && to == null) {
+            return new ArrayList<>(findAll());
+        }
+
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Vuelo> cq = cb.createQuery(Vuelo.class);
+            Root<Vuelo> vuelo = cq.from(Vuelo.class);
+
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (from != null) {
+                predicates.add(cb.greaterThanOrEqualTo(vuelo.get("salida"), from));
+            }
+
+            if (to != null) {
+                predicates.add(cb.lessThanOrEqualTo(vuelo.get("salida"), to));
+            }
+
+            cq.where(predicates.toArray(new Predicate[0]));
+
+            List<Vuelo> result = em.createQuery(cq).getResultList();
+
+            return new ArrayList<>(result);
+        } catch (Exception e) {
+            System.out.println("Hubo un error buscando vuelos: " + e.getMessage());
+            return new ArrayList<>();
+        }
+    }
 }

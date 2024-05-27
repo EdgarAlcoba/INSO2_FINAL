f6415b23b1a7996ea66be8002ab6dde211f9c412
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-05-27 19:51:35 +0200

    Creado controlador de Home de usuario, falta las rutas reales

---
+++ src/main/java/ControllerClient/ClientHomeBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControllerClient;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author extre
+ */
+@ManagedBean (name = "clientHomeBean")
+@ViewScoped
+public class ClientHomeBean {
+    private String currentView = "";
+
+    public String getCurrentView() {
+        return currentView;
+    }
+
+    public void setCurrentView(String currentView) {
+        this.currentView = currentView;
+    }
+    
+    public void setView(String view){
+        switch(view){
+            case "flightSearch":
+                currentView = "flightSearch.xhtml";
+                break;
+            case "miFlights":
+                currentView = ("flightSearch.xhtml");
+                break;
+            case "miProfile":
+                currentView = ("flightSearch.xhtml");
+                break;
+        }
+    }
+    
+}

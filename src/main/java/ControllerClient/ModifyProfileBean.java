b1b9b9b70fafef8d598f554a28c165f7454f18ac
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-05-30 16:10:49 +0200

    Terminado controlador de modificar usuario

--- src/main/java/ControllerClient/ModifyProfileBean.java
+++ src/main/java/ControllerClient/ModifyProfileBean.java
@@ -6,8 +6,7 @@
 package ControllerClient;
 
 import EJB.UsuarioFacadeLocal;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import es.unileon.inso2.aerolinea.exceptions.EditUserException;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
@@ -26,14 +25,15 @@ public class ModifyProfileBean {
 
     private Usuario user;
     private String confirmPassword;
-
-    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    private String oldEmail;
+    private String oldPassword;
 
     @PostConstruct
     public void init() {
         Usuario thisUser = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user");
         setUser(thisUser);
+        this.oldEmail = thisUser.getCorreo();
+        this.oldPassword = thisUser.getContrasena();
     }
 
     @EJB
@@ -54,40 +54,34 @@ public class ModifyProfileBean {
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword = confirmPassword;
     }
-
+    
     public void saveData() {
         if (this.user.getContrasena() != null && this.confirmPassword != null) {
             if (!checkPassword()) {
                 showAlert(2, "Error en la contraseña", "Las contraseñas introducidas no coinciden");
-            } else if (!isValidEmail()) {
-                showAlert(2, "Error en el correo", "El correo introducido no es un correo valido");
             } else {
-                this.UFL.edit(this.user);
+                try {
+                    this.UFL.editUsuario(this.user, this.oldEmail, this.oldPassword);
+                    showAlert(3, "Usuario actualizado", "El usuario ha sido modificado correctamente");
+                } catch (EditUserException e) {
+                    showAlert(2, "Error al actualizar el usuario", e.getMessage());
+                }
+
             }
-        } else if (!isValidEmail()) {
-            showAlert(2, "Error en el correo", "El correo introducido no es un correo valido");
         } else {
+            showAlert(3, "Usuario actualizado", "El usuario ha sido modificado correctamente");
             this.UFL.edit(this.user);
         }
     }
 
-private boolean isValidEmail() {
-        if (this.user.getCorreo() == null) {
-            return false;
-        }
-        Matcher matcher = EMAIL_PATTERN.matcher(this.user.getCorreo());
-        return matcher.matches();
-    }
-
     private void showAlert(int severity, String name, String text) {
-        System.out.println(">Llega aqui");
         FacesContext context = FacesContext.getCurrentInstance();
         switch (severity) {
             case 2:
                 context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, name, text));
                 break;
             case 3:
-                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, name, text));
+                context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, name, text));
                 break;
         }
     }

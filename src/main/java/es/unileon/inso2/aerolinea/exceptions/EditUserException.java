a5ecda92a4310af48ee2998f8e89e74565c177e3
Víctor Martínez <vmartc06@estudiantes.unileon.es>
2024-05-30 15:47:30 +0200

    Added exceptions

---
+++ src/main/java/es/unileon/inso2/aerolinea/exceptions/EditUserException.java
@@ -0,0 +1,7 @@
+package es.unileon.inso2.aerolinea.exceptions;
+
+public class EditUserException extends Exception {
+    public EditUserException(String message) {
+        super(message);
+    }
+}

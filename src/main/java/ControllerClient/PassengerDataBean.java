d4694f9b4fca3b7fd1694e853005d756811b0e61
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-06-01 11:48:54 +0200

    Terminado controlador para introducir los datos del pasajero

--- src/main/java/ControllerClient/PassengerDataBean.java
+++ src/main/java/ControllerClient/PassengerDataBean.java
@@ -5,8 +5,16 @@
  */
 package ControllerClient;
 
+import EJB.PasajeroFacadeLocal;
+import es.unileon.inso2.aerolinea.exceptions.CreatePassengerException;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import modelo.Pasajero;
 
 /**
@@ -16,7 +24,27 @@ import modelo.Pasajero;
 @ManagedBean
 @ViewScoped
 public class PassengerDataBean {
+
     private Pasajero pasajero;
+
+    private ArrayList<Pasajero> pasajeros = new ArrayList<>();
+
+    @ManagedProperty(value = "#{clientHomeBean}")
+    private ClientHomeBean CHB;
+
+    @ManagedProperty(value = "#{flightSearchBean}")
+    private FlightSearchBean FSB;
+
+    @EJB
+    private PasajeroFacadeLocal PFL;
+
+    @PostConstruct
+    public void init() {
+        int numPasajeros = this.FSB.getNumberOfPassengers();
+        for (int i = 0; i < numPasajeros; i++) {
+            pasajeros.add(new Pasajero());
+        }
+    }
 
     public Pasajero getPasajero() {
         return pasajero;
@@ -25,6 +53,52 @@ public class PassengerDataBean {
     public void setPasajero(Pasajero pasajero) {
         this.pasajero = pasajero;
     }
+
+    public ArrayList<Pasajero> getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setPasajeros(ArrayList<Pasajero> pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+
+    public ClientHomeBean getCHB() {
+        return CHB;
+    }
+
+    public void setCHB(ClientHomeBean CHB) {
+        this.CHB = CHB;
+    }
+
+    public FlightSearchBean getFSB() {
+        return FSB;
+    }
+
+    public void setFSB(FlightSearchBean FSB) {
+        this.FSB = FSB;
+    }
+
+    public void checkPassengerData() {
+        for (int i = 0; i < this.pasajeros.size(); i++) {
+            try {
+                Pasajero newPasajero = this.PFL.createPasajero(pasajeros.get(i));
+                this.pasajeros.set(i, newPasajero);
+            } catch (CreatePassengerException e) {
+                showAlerts("Error en los datos del pasajero " + (i+1), e.getMessage());
+                return;
+            }
+        }
+        goToReview();
+    }
+
+    public void goToReview() {
+        this.CHB.setCurrentView("orderPreview.xhtml");
+        this.CHB.update();
+    }
     
-    
+    public void showAlerts(String head, String text){
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, head, text));
+    }
+
 }

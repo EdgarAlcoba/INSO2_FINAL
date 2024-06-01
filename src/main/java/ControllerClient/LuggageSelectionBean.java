a734b66410bda1b7533c4359cec5b7efde0bdd4c
Pablomorais121 <pmoraa00@estudiantes.unileon.es>
2024-06-01 13:32:44 +0200

    Terminado de verdad el controlador para seleccionar maletas, faltaba crear la lista de maletas

--- src/main/java/ControllerClient/LuggageSelectionBean.java
+++ src/main/java/ControllerClient/LuggageSelectionBean.java
@@ -5,9 +5,11 @@
  */
 package ControllerClient;
 
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import modelo.Maleta;
 
 /**
  *
@@ -23,6 +25,8 @@ public class LuggageSelectionBean {
     @ManagedProperty (value ="#{clientHomeBean}")
     private ClientHomeBean CHB;
     
+    
+    private ArrayList<Maleta> luggage = new ArrayList<>();
 
     public int getLight() {
         return light;
@@ -55,11 +59,37 @@ public class LuggageSelectionBean {
     public void setCHB(ClientHomeBean CHB) {
         this.CHB = CHB;
     }
+
+    public ArrayList<Maleta> getLuggage() {
+        return luggage;
+    }
+
+    public void setLuggage(ArrayList<Maleta> luggage) {
+        this.luggage = luggage;
+    }
+    
+    public void createLuggage(){
+        for(int i = 0; i < this.light; i++){
+            Maleta ligero = new Maleta();
+            ligero.setPesoKg(12);
+            this.luggage.add(ligero);
+        }
+        for(int i = 0; i < this.normal; i++){
+            Maleta comun = new Maleta();
+            comun.setPesoKg(20);
+            this.luggage.add(comun);
+        }
+        for(int i = 0; i < this.heavy; i++){
+            Maleta pesado = new Maleta();
+            pesado.setPesoKg(25);
+            this.luggage.add(pesado);
+        }
+    }
     
     public void passengerData(){
+        this.createLuggage();
         this.CHB.setCurrentView("passengerData.xhtml");
         this.CHB.update();
     }
-    
     
 }
